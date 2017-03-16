f4afeed28b8e5a4677a245af5de9ac2bc818c980
tomokowitz <tomokowitz@gmail.com>
2017-03-15 21:54:24 -0400

    csc 516 hw 6

---
+++ okowitz6/classes/.jsps/_okowitz6.java
@@ -0,0 +1,90 @@
+
+import oracle.jsp.runtime.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import oracle.jsp.el.*;
+import javax.servlet.jsp.el.*;
+import java.io.CharArrayWriter;
+import java.io.PrintWriter;
+
+
+public class _okowitz6 extends com.orionserver.http.OrionHttpJspPage {
+
+
+  // ** Begin Declarations
+
+
+  // ** End Declarations
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response) throws java.io.IOException, ServletException {
+
+    response.setContentType( "text/html;charset=windows-1252");
+    /* set up the intrinsic variables using the pageContext goober:
+    ** session = HttpSession
+    ** application = ServletContext
+    ** out = JspWriter
+    ** page = this
+    ** config = ServletConfig
+    ** all session/app beans declared in globals.jsa
+    */
+    PageContext pageContext = JspFactory.getDefaultFactory().getPageContext( this, request, response, null, true, JspWriter.DEFAULT_BUFFER, true);
+    // Note: this is not emitted if the session directive == false
+    HttpSession session = pageContext.getSession();
+    int __jsp_tag_starteval;
+    ServletContext application = pageContext.getServletContext();
+    JspWriter out = pageContext.getOut();
+    _okowitz6 page = this;
+    ServletConfig config = pageContext.getServletConfig();
+    javax.servlet.jsp.el.VariableResolver __ojsp_varRes = (VariableResolver)new OracleVariableResolverImpl(pageContext);
+    Throwable exception = (Throwable) request.getAttribute(PageContext.EXCEPTION);
+
+    try {
+
+
+      out.write(__oracle_jsp_text[0]);
+      out.write(__oracle_jsp_text[1]);
+      
+            if (exception != null) 
+            { 
+              out.println(exception.getMessage());
+              CharArrayWriter charArrayWriter = new CharArrayWriter(); 
+              PrintWriter printWriter = new PrintWriter(charArrayWriter, true); 
+              exception.printStackTrace(printWriter); 
+              out.println(charArrayWriter.toString()); 
+            } 
+          
+      out.write(__oracle_jsp_text[2]);
+
+    }
+    catch (Throwable e) {
+      if (!(e instanceof javax.servlet.jsp.SkipPageException)){
+        try {
+          if (out != null) out.clear();
+        }
+        catch (Exception clearException) {
+        }
+        pageContext.handlePageException(e);
+      }
+    }
+    finally {
+      OracleJspRuntime.extraHandlePCFinally(pageContext, true);
+      JspFactory.getDefaultFactory().releasePageContext(pageContext);
+    }
+
+  }
+  private static final char __oracle_jsp_text[][]=new char[3][];
+  static {
+    try {
+    __oracle_jsp_text[0] = 
+    "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n\"http://www.w3.org/TR/html4/loose.dtd\">\n".toCharArray();
+    __oracle_jsp_text[1] = 
+    "\n<html>\n  <head>\n    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\"/>\n    <title>okowitz6</title>\n    <style type=\"text/css\">\n      body {\n      background-color: #ffde73; \n}\n    </style>\n  </head>\n  <body>An error occured:<br/><pre>\n    ".toCharArray();
+    __oracle_jsp_text[2] = 
+    "\n    </pre></body>\n</html>".toCharArray();
+    }
+    catch (Throwable th) {
+      System.err.println(th);
+    }
+}
+}
