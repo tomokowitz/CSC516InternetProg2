ab1b62916ba3a26fb6ead7f45e8ad8292601606a
tomokowitz <tomokowitz@gmail.com>
2017-03-24 21:32:48 -0400

    csc hw 7 and bella's web page

---
+++ okowitz7Application/okowitz7JSTL/okowitz7/src/okowitz7.java
@@ -0,0 +1,43 @@
+public class okowitz7 {
+    String name;
+    int cakes_sold;
+    double delivery_fee;
+    double total; 
+
+
+    public okowitz7() {
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCakes_sold(Integer cakes_sold) {
+        this.cakes_sold = cakes_sold;
+    }
+
+    public Integer getCakes_sold() {
+        return cakes_sold;
+    }
+
+    public void setDelivery_fee(Number delivery_fee) {
+        this.delivery_fee = delivery_fee;
+    }
+
+    public Number getDelivery_fee() {
+        return delivery_fee;
+    }
+
+    public void setTotal(Number total) {
+        this.total = total;
+    }
+
+    public Number getTotal() {
+        return total;
+    }
+}
