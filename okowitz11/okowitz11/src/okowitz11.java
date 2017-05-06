acece395552e039f76deb7a51377fd5d8d2bd8a1
tomokowitz <tomokowitz@gmail.com>
2017-05-06 11:38:00 -0400

    hw 11 csc 516

---
+++ okowitz11/okowitz11/src/okowitz11.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class okowitz11 extends HttpServlet {
+    private static final String CONTENT_TYPE = "text/html; charset=windows-1252";
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    /**Process the HTTP doGet request.
+     */
+    public void doGet(HttpServletRequest request, 
+                      HttpServletResponse response) throws ServletException, IOException {response.setContentType(CONTENT_TYPE);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>okowitz11</title></head>");
+        out.println("<body>");
+        out.println("<p>The servlet has received a GET. This is the reply.</p>");
+        out.println("</body></html>");
+        out.close();
+    }
+
+    /**Process the HTTP doPost request.
+     */
+    public void doPost(HttpServletRequest request, 
+                       HttpServletResponse response) throws ServletException, IOException {response.setContentType(CONTENT_TYPE);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>okowitz11</title></head>");
+        out.println("<body>");
+        out.println("<p>The servlet has received a POST. This is the reply.</p>");
+        out.println("</body></html>");
+        out.close();
+    }
+}
