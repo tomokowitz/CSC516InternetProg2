734ab3dde77ff0284393f4e3c797543c763eff4d
tomokowitz <tomokowitz@gmail.com>
2017-05-10 10:45:23 -0400

    hw 12 action

---
+++ okowitz12/okowitz12/okowitz12/src/okowitz12/okowitz12Action.java
@@ -0,0 +1,10 @@
+package okowitz12;
+
+import org.apache.struts.action.Action;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class okowitz12Action extends Action {
+    public okowitz12Action() {
+    }
+}
