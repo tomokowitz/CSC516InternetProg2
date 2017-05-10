28ab983ba1253ce7a609261b5e22622212071f8b
tomokowitz <tomokowitz@gmail.com>
2017-05-10 15:19:48 -0400

    hw 12 prep

--- okowitz12/okowitz12/okowitz12/src/okowitz12/okowitz12Action.java
+++ okowitz12/okowitz12/okowitz12/src/okowitz12/okowitz12Action.java
@@ -4,7 +4,50 @@ import org.apache.struts.action.Action;
 import javax.servlet.http.*;
 import java.sql.*;
 
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
 public class okowitz12Action extends Action {
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form, 
+                                 HttpServletRequest request, 
+                                 HttpServletResponse response) 
+                                 throws Exception 
+    {
+         PreparedStatement stmt;
+         
+         
+         
+         
+                                 
+         return (mapping.findForward(" ")); 
+         return (mapping.findForward(" ")); 
+         
+         
+         
+         String url = "jdbc:odbc:bakery";
+         Connection con;
+         String query = 
+         
+         
+         return (mapping.findForward("okowitz12success")); 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 
+     }
     public okowitz12Action() {
     }
 }
