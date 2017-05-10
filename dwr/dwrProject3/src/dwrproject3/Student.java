9a7b8693adedf5186344244925fe1d07e50202b6
tomokowitz <tomokowitz@gmail.com>
2017-05-09 20:26:56 -0400

    hw 11

--- dwr/dwrProject3/src/dwrproject3/Student.java
+++ dwr/dwrProject3/src/dwrproject3/Student.java
@@ -10,14 +10,14 @@ public class Student {
     private String major;
     private String address;
     private String city;
-    private String state;   
-    
+    private String state;
+
     public Student() {
     }
-    
-    public Student(String userid, String password, String firstName, String lastName, String phone, String dob,  
-                    String major, String address, String city, String state)
-    {
+
+    public Student(String userid, String password, String firstName, 
+                   String lastName, String phone, String dob, String major, 
+                   String address, String city, String state) {
         this.userid = userid;
         this.password = password;
         this.firstName = firstName;
@@ -37,12 +37,12 @@ public class Student {
 
     public String getPassword() {
         return password;
-    }        
+    }
 
     public String getLastName() {
         return lastName;
     }
-    
+
     public String getFirstName() {
         return firstName;
     }
@@ -54,7 +54,7 @@ public class Student {
     public String getDob() {
         return dob;
     }
-    
+
     public String getMajor() {
         return major;
     }
@@ -62,54 +62,54 @@ public class Student {
     public String getAddress() {
         return address;
     }
-    
+
     public String getCity() {
         return city;
     }
 
-     public String getState() {
+    public String getState() {
         return state;
     }
-    
+
     public void setUserid(String p) {
         userid = p;
     }
-    
+
     public void setPassword(String p) {
         password = p;
     }
-    
+
     public void setFirstName(String p) {
         firstName = p;
     }
-    
+
     public void setLastName(String p) {
         lastName = p;
     }
-    
+
     public void setDob(String p) {
         dob = p;
     }
-    
+
     public void setPhone(String p) {
         phone = p;
     }
-    
+
     public void setMajor(String p) {
         major = p;
     }
-    
+
     public void setAddress(String p) {
         address = p;
-    }   
-    
+    }
+
     public void setCity(String p) {
         city = p;
     }
-    
+
     public void setState(String p) {
         state = p;
-    }    
+    }
 
 }
 
--- dwr/dwrProject3/src/dwrproject3/ex3.java
+++ dwr/dwrProject3/src/dwrproject3/ex3.java
@@ -7,108 +7,91 @@ import java.util.ArrayList;
 public class ex3 {
 
     public Student getStudent(String userid, String password) {
-        Student student = new Student(userid, password, "Not Found", "", "", "", "", 
-                                      "", "", "");
+        Student student = 
+            new Student(userid, password, "Not Found", "", "", "", "", "", "", 
+                        "");
         String connection_string = "jdbc:odbc:classes";
         Connection con;
-        try
-        {
-          Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        } catch (java.lang.ClassNotFoundException e) {
+            return student;
         }
-        catch(java.lang.ClassNotFoundException e)
-        {
-          return student;
+        try {
+            con = DriverManager.getConnection(connection_string, "", "");
+            String query = 
+                "SELECT FIRSTNAME, LASTNAME, PHONE, DOB, MAJOR, ADDRESS, " + 
+                "CITY, STATE " + 
+                "FROM STUDENTS WHERE USERID = ? AND PASSWORD = ?";
+            PreparedStatement s = con.prepareStatement(query);
+            s.setString(1, userid);
+            s.setString(2, password);
+            ResultSet rs = s.executeQuery();
+            if (rs.next()) {
+                student = 
+                        new Student(userid, password, rs.getString(1), rs.getString(2), 
+                                    rs.getString(3), rs.getString(4), 
+                                    rs.getString(5), rs.getString(6), 
+                                    rs.getString(7), rs.getString(8));
+            }
+        } catch (Exception e) {
         }
-        try
-        {
-          con = DriverManager.getConnection (connection_string,"","");
-          String query = 
-            "SELECT FIRSTNAME, LASTNAME, PHONE, DOB, MAJOR, ADDRESS, " +
-            "CITY, STATE " +
-            "FROM STUDENTS WHERE USERID = ? AND PASSWORD = ?";
-          PreparedStatement s = con.prepareStatement(query);
-          s.setString(1, userid);
-          s.setString(2, password);
-          ResultSet rs = s.executeQuery();
-          if (rs.next())
-          {
-            student = new Student( userid, password, rs.getString(1), rs.getString(2), 
-                        rs.getString(3), rs.getString(4), rs.getString(5), 
-                        rs.getString(6), rs.getString(7), rs.getString(8)); 
-          }
-        }
-        catch (Exception e)
-        {
-        }
-        return student; 
+        return student;
     }
-    
+
     public ArrayList getStudents() {
         ArrayList students = new ArrayList();
         String connection_string = "jdbc:odbc:classes";
         Connection con;
-        try
-        {
-          Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        } catch (java.lang.ClassNotFoundException e) {
+            return students;
         }
-        catch(java.lang.ClassNotFoundException e)
-        {
-          return students;
+        try {
+            con = DriverManager.getConnection(connection_string, "", "");
+            String query = "SELECT * FROM STUDENTS";
+            PreparedStatement s = con.prepareStatement(query);
+            ResultSet rs = s.executeQuery();
+            while (rs.next()) {
+                students.add(new Student(rs.getString(1), rs.getString(2), 
+                                         rs.getString(3), rs.getString(4), 
+                                         rs.getString(5), rs.getString(6), 
+                                         rs.getString(7), rs.getString(8), 
+                                         rs.getString(9), rs.getString(10)));
+            }
+        } catch (Exception e) {
         }
-        try
-        {
-          con = DriverManager.getConnection (connection_string,"","");
-          String query = 
-            "SELECT * FROM STUDENTS";
-          PreparedStatement s = con.prepareStatement(query);
-          ResultSet rs = s.executeQuery();
-          while (rs.next())
-          {
-            students.add( new Student( rs.getString(1), rs.getString(2), 
-                        rs.getString(3), rs.getString(4), rs.getString(5), 
-                        rs.getString(6), rs.getString(7), rs.getString(8),
-                        rs.getString(9), rs.getString(10)));
-          }
-        }
-        catch (Exception e)
-        {
-        }  
         return students;
     }
-    
-    public void setStudent(Student student) {  
+
+    public void setStudent(Student student) {
         String connection_string = "jdbc:odbc:classes";
         Connection con;
-        try
-        {
-          Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        } catch (java.lang.ClassNotFoundException e) {
+            return;
         }
-        catch(java.lang.ClassNotFoundException e)
-        {
-          return;
-        }
-        try
-        {
-          con = DriverManager.getConnection (connection_string,"","");
-          String query = 
-            "UPDATE STUDENTS SET FIRSTNAME = ?, LASTNAME = ?, PHONE = ?, " +
-             "DOB = ?, MAJOR = ?, ADDRESS = ?, CITY = ?, STATE = ? " +
-             "WHERE USERID = ?";
-          PreparedStatement s = con.prepareStatement(query);
-          s.setString(9, student.getUserid());
-          s.setString(1, student.getFirstName());
-          s.setString(2, student.getLastName());
-          s.setString(3, student.getPhone());
-          s.setString(4, student.getDob());
-          s.setString(5, student.getMajor());
-          s.setString(6, student.getAddress());
-          s.setString(7, student.getCity());
-          s.setString(8, student.getState());
-          s.executeUpdate();
-        }
-        catch (Exception e)
-        {
+        try {
+            con = DriverManager.getConnection(connection_string, "", "");
+            String query = 
+                "UPDATE STUDENTS SET FIRSTNAME = ?, LASTNAME = ?, PHONE = ?, " + 
+                "DOB = ?, MAJOR = ?, ADDRESS = ?, CITY = ?, STATE = ? " + 
+                "WHERE USERID = ?";
+            PreparedStatement s = con.prepareStatement(query);
+            s.setString(9, student.getUserid());
+            s.setString(1, student.getFirstName());
+            s.setString(2, student.getLastName());
+            s.setString(3, student.getPhone());
+            s.setString(4, student.getDob());
+            s.setString(5, student.getMajor());
+            s.setString(6, student.getAddress());
+            s.setString(7, student.getCity());
+            s.setString(8, student.getState());
+            s.executeUpdate();
+        } catch (Exception e) {
             e.printStackTrace();
-        }       
+        }
     }
 }
