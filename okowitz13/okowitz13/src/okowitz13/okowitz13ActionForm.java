1d2e28a5b617fc8284b310f5f3789faeabf5dc5c
tomokowitz <tomokowitz@gmail.com>
2017-05-12 21:57:25 -0400

    hw 13

--- okowitz13/okowitz13/src/okowitz13/okowitz13ActionForm.java
+++ okowitz13/okowitz13/src/okowitz13/okowitz13ActionForm.java
@@ -1,10 +1,46 @@
-package okowitz13;
+package okowitz12;
 
 import org.apache.struts.action.*;
 
 public class okowitz13ActionForm extends ActionForm  {
 
-
+    int orderID;
+    String name;
+    String location;
+    int customerID;
+    
     public okowitz13ActionForm() {
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
     }
 }
